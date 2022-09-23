76d1f27147be1a1a46f7ad2230de1969a063b1c3
Fabrício M. Damasceno <fabricio.damasceno@outlook.com.br>
2022-09-22 23:04:56 -0300

    Versionando Projeto

---
+++ resources/js/Pages/About.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <h1>Sobre nós</h1>
+        <h2>Olá, {{ name }}</h2>
+        <h2>Estou fazendo curso de {{ curso}}</h2>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'PageAbout',
+
+    props: {
+        name: String,
+        curso: String
+    },
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,33 @@
+<template>
+    <table class="table table-hover table-striped">
+        <thead>
+            <tr>
+                <th scope="col">ID</th>
+                <th scope="col">Título</th>
+                <th scope="col">Descrição</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr v-for="post in posts" :key="post.id">
+                <th scope="row">{{ post.id }}</th>
+                <td>{{ post.title }}</td>
+                <td>{{ post.content }}</td>
+            </tr>
+        </tbody>
+    </table>
+</template>
+
+<script>
+export default {
+    name: 'PageHome',
+
+    props: {
+        posts: Object
+    },
+
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
